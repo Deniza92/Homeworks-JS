d7c92f960097643b3de967e03422cca7c653d8b8
Deniza92 <151684399+Deniza92@users.noreply.github.com>
2023-12-06 23:54:04 +0100

    Add files via upload

---
+++ Homework js1/app.js
@@ -0,0 +1,8 @@
+console.log ("Its working")
+//Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+
+let onePhonePrice = 119.95;
+let taxRate = 5 / 100;
+let priceOfPhones = 30;
+
+console.log(onePhonePrice = onePhonePrice * taxRate) * priceOfPhones
